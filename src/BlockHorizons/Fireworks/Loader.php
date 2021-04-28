a8379fd98bb2046bd66839352d2cd6d0e6eb1527
Glyphs31 <83299753+Glyphs31@users.noreply.github.com>
2021-04-28 16:23:20 +0700

    Added Fireworks Charge

--- src/BlockHorizons/Fireworks/Loader.php
+++ src/BlockHorizons/Fireworks/Loader.php
@@ -16,6 +16,7 @@ class Loader extends PluginBase {
 	public function onEnable(): void
 	{
 		ItemFactory::registerItem(new Fireworks(), true);
+		ItemFactory::registerItem(new Item(Item::FIREWORKS_CHARGE, 0, "Fireworks Charge"), true);
 		Item::initCreativeItems(); //will load firework rockets from pocketmine's resources folder
 		if (!Entity::registerEntity(FireworksRocket::class, false, ["FireworksRocket", "minecraft:fireworks_rocket"])) {
 			$this->getLogger()->error("Failed to register FireworksRocket entity with savename 'FireworksRocket'");
